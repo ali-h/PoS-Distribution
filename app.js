b79f8f9784a16825f4fd1c5bbff04826a2de80fc
ali-h <iamahaalihassanarain@gmail.com>
2019-07-16 18:44:25 +0500

    distribution functions

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const config = require('./config.json')
 const fs = require("fs")
 const axios = require('axios')
 const memo_temp = fs.readFileSync("./assets/memo.md", "utf-8")
-
+steem.api.setOptions({
+    url: 'https://anyx.io'
+})
 function getAllStakers (callback) {
     var stakers = []
     var requestURL = "https://api.steem-engine.com/rpc/contracts"
@@ -99,88 +101,91 @@ function sendRewards(stakers, callback) {
         month: '2-digit',
         year: 'numeric',
     }))
-    var jsonOBJ = {
-        "0" : []
+    var JSONsPerBlock = 25
+    var total_batches = Math.ceil(stakers.length / JSONsPerBlock)
+    var jsonARR = []
+    for (var i = 0; i <= total_batches - 1; i++) {
+        jsonARR[i] = []
     }
-    var num = 0
-    for(var i = 0; i <= stakers.length - 1; i++) {
-        if (i%11 == 0 && i>1) {
-            num++
-            jsonOBJ[num] = []
+    var batch = 0
+    for (var x = 0; x <= stakers.length - 1; x++) {
+        if (x % JSONsPerBlock == 0 && x > 1) {
+            batch++
         }
         var json = 	{
             "contractName":"tokens",
             "contractAction":"transfer",
             "contractPayload":{
                 "symbol": config.token.symbol,
-                "to": stakers.username,
-                "quantity": stakers.reward,
+                "to": stakers[x].username,
+                "quantity": stakers[x].reward,
                 "memo": memo
             }
         }
-        jsonOBJ[num].push(json)
+        jsonARR[batch].push(json)
     }
-    console.log(jsonOBJ)
-    // steem.broadcast.customJson(config.keys.active, [config.username], [], "ssc-mainnet1", JSON.stringify(json), function(err, result) {
-    //     if (!err) {
-    //         callback(true)
-    //     }
-    //     else {
-    //         console.log("ERR".bgRed, "While Sending Rewards".yellow)
-    //         callback(false)
-    //     }
-    // })
+    var transferLOG = []
+    for (var batch_no = 0; batch_no <= jsonARR.length - 1; batch_no++) {
+        steem.broadcast.customJson(config.keys.active, [config.username], [], "ssc-mainnet1", JSON.stringify(jsonARR[batch_no]), function(err, result) {
+            if (!err) {
+                console.log("Distribution Done for the Batch No [".yellow, batch_no.yellow, "]")
+                // callback(true)
+            }
+            else {
+                console.log("ERR".bgRed, "While Sending Rewards to #Batch No [".yellow, batch_no.yellow ,"]".yellow)
+                // callback(false)
+            }
+        })
+    }    
 }
 
 function InitiateDistribution () {
-    setTimeout(function () {    
-        console.log("DISTRIBUTION WAS CALLED, CURRENT TIME = ".yellow, new Date().toLocaleString(undefined, {
-            hour: '2-digit',
-            minute: '2-digit',
-            second: '2-digit'
-        }).green)
-        console.log("NOW GETTING ALL STAKERS".yellow)
-        console.log("")
-        getAllStakers(function(stakers) {
-            if (stakers.length > 0) {
-                getRewards(stakers, function(rewards) {
-                    console.log("---------------------------------------------------".yellow)
-                    console.log("TOTAL STAKERS  : ".yellow, colors.green(rewards.info.stakers))
-                    console.log("REWARD POOL    : ".yellow, colors.green(rewards.info.reward_pool))
-                    console.log("TOTAL STAKE    : ".yellow, colors.green(rewards.info.total_stake))
-                    console.log("AVERAGE REWARD : ".yellow, colors.green(rewards.info.average_reward))
-                    console.log("---------------------------------------------------".yellow)
-                    console.log("")
-                    console.log("NOW SENDING REWARDS, PLEASE WAIT".yellow)
-                    sendRewards(rewards.details, function (result) {
-                        if (!result) {
-                            console.log ("SOMETHING BAD HAPPENED".yellow)
-                        }
-                        else {
-                            console.log("SUCCESS".green)
-                            console.log("LOG FOR THIS DISTRIBUTION CAN BE FOUND IN DISTRIBUTIONS FOLDER".green)
-                            console.log("")
-                        }
-                        setTimeout (function () {      
-                            console.log("TIMER WILL AGAIN START IN 10 MINUTES".yellow)
-                            console.log("")
-                            setTimeout(function () {
-                                initTimer()
-                            }, 10 * 60 * 1000)
-                        }, 1000)
-                    })
-                })
-            }
-            else {
-                console.log("ERR".bgRed, "NO ONE IS STAKING".yellow, colors.green(config.token.symbol), "TOKEN, DISTRIBUTION FOR THIS DAY SKIPPED".yellow)
-                console.log("TIMER WILL BEGIN IN 10 MINUTES".yellow)
+    console.log("DISTRIBUTION WAS CALLED, CURRENT TIME = ".yellow, new Date().toLocaleString(undefined, {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    }).green)
+    console.log("NOW GETTING ALL STAKERS".yellow)
+    console.log("")
+    getAllStakers(function(stakers) {
+        if (stakers.length > 0) {
+            getRewards(stakers, function(rewards) {
+                console.log("---------------------------------------------------".yellow)
+                console.log("TOTAL STAKERS  : ".yellow, colors.green(rewards.info.stakers))
+                console.log("REWARD POOL    : ".yellow, colors.green(rewards.info.reward_pool))
+                console.log("TOTAL STAKE    : ".yellow, colors.green(rewards.info.total_stake))
+                console.log("AVERAGE REWARD : ".yellow, colors.green(rewards.info.average_reward))
+                console.log("---------------------------------------------------".yellow)
                 console.log("")
-                setTimeout(function () {
-                    initTimer()
-                }, 10 * 60 * 1000)
-            }
-        })
-    }, 2000)
+                console.log("NOW SENDING REWARDS, PLEASE WAIT".yellow)
+                sendRewards(rewards.details, function (result) {
+                    if (!result) {
+                        console.log ("SOMETHING BAD HAPPENED".yellow)
+                    }
+                    else {
+                        console.log("SUCCESS".green)
+                        console.log("LOG FOR THIS DISTRIBUTION CAN BE FOUND IN DISTRIBUTIONS FOLDER".green)
+                        console.log("")
+                    }
+                    setTimeout (function () {      
+                        console.log("TIMER WILL AGAIN START IN 10 MINUTES".yellow)
+                        console.log("")
+                        setTimeout(function () {
+                            initTimer()
+                        }, 10 * 60 * 1000)
+                    }, 1000)
+                })
+            })
+        }
+        else {
+            console.log("ERR".bgRed, "NO ONE IS STAKING".yellow, colors.green(config.token.symbol), "TOKEN, DISTRIBUTION FOR THIS DAY SKIPPED".yellow)
+            console.log("TIMER WILL BEGIN IN 10 MINUTES".yellow)
+            console.log("")
+            setTimeout(function () {
+                initTimer()
+            }, 10 * 60 * 1000)
+        }
+    })
 }
 
 console.log('\033[2J')
