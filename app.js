4719f94f5813186faca5b9507b64fb440a2e5e7a
Ali H <34513931+alihassanah@users.noreply.github.com>
2019-07-17 13:50:05 +0500

    Distribution to 10 minutes after the hour passes

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ function initTimer () {
             })
             process.stdout.write("\rCURRENT LOCAL TIME : ".yellow + currentTime.green)
             var currentHour = new Date().getHours()
-            if (currentHour == config.distribution_hour) {
+            if (currentHour == config.distribution_hour && new Date().getMinutes() < 10) {
                 clearInterval(TimeTimer)
                 console.log("\n")
                 InitiateDistribution()
